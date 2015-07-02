20fed2046bcf6bb99f0697e502847e1b57df6628
Jason Zou <jason02px2018@jason-zou.local>
2015-07-02 13:55:03 -0500

    Initial Commit

---
+++ Tic Tac ToeTests/Tic_Tac_ToeTests.swift
@@ -0,0 +1,35 @@
+//
+//  Tic_Tac_ToeTests.swift
+//  Tic Tac ToeTests
+//
+//  Created by Jason Zou on 7/2/15.
+//  Copyright © 2015 Jason Zou. All rights reserved.
+//
+
+import XCTest
+
+class Tic_Tac_ToeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Tic Tac ToeUITests/Tic_Tac_ToeUITests.swift
@@ -0,0 +1,35 @@
+//
+//  Tic_Tac_ToeUITests.swift
+//  Tic Tac ToeUITests
+//
+//  Created by Jason Zou on 7/2/15.
+//  Copyright © 2015 Jason Zou. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class Tic_Tac_ToeUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
